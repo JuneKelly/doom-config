4845f38b920fa4d44afab2ef6eaa99fba34de71f
June Kelly <june@cnjctr.com>
2026-04-28 09:37:08 +0100

    Catppuccin mocha

--- config.el
+++ config.el
@@ -32,7 +32,7 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 
-(setq catppuccin-flavor 'macchiato) ;; Options: 'frappe, 'latte, 'macchiato, or 'mocha
+(setq catppuccin-flavor 'mocha) ;; Options: 'frappe, 'latte, 'macchiato, or 'mocha
 (setq doom-theme 'catppuccin)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
