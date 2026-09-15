0b9af48a10a18cf5775e6845cbd6ab3f210faee2
June Kelly <june@cnjctr.com>
2026-09-15 19:19:48 +0100

    Monaspace Xenon as variable-pitch font

--- config.el
+++ config.el
@@ -23,7 +23,7 @@
 ;;
 
 (setq doom-font (font-spec :family "Monaspace Xenon" :size 14 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Helvetica" :size 14))
+      doom-variable-pitch-font (font-spec :family "Monaspace Xenon" :size 13 :weight 'semi-light))
 
 
 ;;
